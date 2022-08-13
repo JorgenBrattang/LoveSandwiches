5f177a49a8bda1dfcc125becff8d325c5638de60
JorgenBrattang <jorgen.kvist@gmail.com>
2022-08-13 09:07:45 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -19,23 +19,21 @@ def get_sales_data():
     Get sales figures input from the user.
     Run a while loop to collect a valid string of data from the user
     via the terminal, which must be a string of 6 numbers separated
-    by commas. The loop wil repeatedly request data, until it is valid.
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
-        print("Example, 10,20,30,40,50,60\n")
+        print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-        # print(f"The data provided is {data_str}")
 
         sales_data = data_str.split(",")
 
-        # If the validate_data is valid, it will return True to end the while loop.
         if validate_data(sales_data):
             print("Data is valid!")
             break
-    
+
     return sales_data
 
 
@@ -60,12 +58,10 @@ def validate_data(values):
 
 def update_sales_worksheet(data):
     """
-    Update sales worksheet, add new row with the list data provided.
+    Update sales worksheet, add new row with the list data provided
     """
-    print("Updating sales worksheet....\n")
-    # Accesses the google worksheet
+    print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
-    # Appends a new row with the data acquired from the user
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
@@ -77,26 +73,30 @@ def calculate_surplus_data(sales_row):
     The surplus is defined as the sales figure subtracted from the stock:
     - Positive surplus indicates waste
     - Negative surplus indicates extra made when stock was sold out.
-     """
+    """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    pprint(stock_row)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 
 def main():
     """
-    Run all program function
+    Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
-
-
-
 
